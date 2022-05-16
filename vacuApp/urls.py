af2278bd8b7366889d8db6fb7dfde8f2c048ef64
Gonzalez Aguilar Santiago <zant.aguilar@gmail.com>
2022-05-16 14:37:25 -0300

    Creada carpeta templates,registerFiebreA,registerCentro

--- vacuApp/urls.py
+++ vacuApp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('register',views.register,name='register'),
     path('',views.home,name='home'),
     path('registerCovid',views.registerCovid,name="registerCovid"),
-    path('registerGripe',views.registerGripe,name="registerGripe")
+    path('registerGripe',views.registerGripe,name="registerGripe"),
+    path('registerFiebreA',views.registerFiebreA,name="registerFiebreA"),
+    path('registerCentro',views.registerCentro,name="registerCentro"),
 ]
--- vacuApp/views.py
+++ vacuApp/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def register(response):
-    return render(response,'register.html')
+    return render(response,'register/register.html')
 
 
 def home(response):
     return render(response,'home.html')
 
 def registerCovid(response):
-    return render(response,'registerCovid.html')
+    return render(response,'register/registerCovid.html')
 
 def registerGripe(response):
-    return render(response,'registerGripe.html')
+    return render(response,'register/registerGripe.html')
+
+def registerFiebreA(response):
+    return render(response,'register/registerFiebreA.html')
+
+def registerCentro(response):
+    return render(response,'register/registerCentro.html')
