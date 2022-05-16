4a93a425e16c5bafd532d717867f99e5af64a98f
marilynolivera <105252740+marilynolivera@users.noreply.github.com>
2022-05-16 19:33:48 -0300

    xd

--- vacuApp/urls.py
+++ vacuApp/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('register',views.testBulma,name='register'),
-    path('',views.home,name='home'),
     path('infoPersonal',views.infoPersonal,name='visualizarInfoPersonal'),
     path('modificarInfo',views.modificarInfo,name='modificarInfoPersonal'),
     path('modContraseña',views.modContraseña,name='modificarContraseña'),
