90a5238d19ee7b12121e95591aba2e64ad4248c7
marilynolivera <105252740+marilynolivera@users.noreply.github.com>
2022-05-15 21:51:34 -0300

    conclui visual sin colores de modificar inr¡formacion personal

--- vacuApp/urls.py
+++ vacuApp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register',views.testBulma,name='register'),
     path('',views.home,name='home'),
     path('infoPersonal',views.infoPersonal,name='visualizarInfoPersonal'),
-    path('infoPersonal',views.modificarInfo,name='modificarInfoPersonal'),
+    path('modificarInfo',views.modificarInfo,name='modificarInfoPersonal'),
 ]
