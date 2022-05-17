fb6e8f897386ed0a47505e0bcdefe93024f304b6
Gonzalez Aguilar Santiago <zant.aguilar@gmail.com>
2022-05-17 20:09:47 -0300

    asd

--- vacuApp/models.py
+++ vacuApp/models.py
@@ -7,16 +7,18 @@ from django.db.models import CharField
 class Center(models.Model):
     name = CharField(max_length=100)
     adress = CharField(max_length=100)
-
+    
+    
     def __str__(self):
-        return str(self.name)
+        return str(self.name)    
+    
 
 class User(models.Model):
     name = CharField(max_length=50)
     surname = CharField(max_length=90)
     email = CharField(max_length=100)
     DNI = CharField(max_length=8,unique=True)
-    birthDate = models.DateField
+    birthDate = models.DateField()
     sex = CharField(max_length=1)
     password = CharField(max_length=20)
     token = CharField(max_length=4,null=True)
