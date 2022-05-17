3e53813148ebef9e1663f1657b4f5abc609aa4b6
marilynolivera <105252740+marilynolivera@users.noreply.github.com>
2022-05-17 12:26:06 -0300

    agregue cosas al login

--- vacuApp/views.py
+++ vacuApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from vacuApp.models import *
 # Create your views here.
+from datetime import date
 
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+    
 def register(response):
     return render(response,'register/register.html')
 
