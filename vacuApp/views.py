53ab6cdde9856b1add7628f425436fc9c85321b8
Gonzalez Aguilar Santiago <zant.aguilar@gmail.com>
2022-05-21 18:09:06 -0300

    commitito

--- vacuApp/views.py
+++ vacuApp/views.py
@@ -231,7 +231,7 @@ def completarUsuario(response):
 
 
 def asignarVacunas(user):
-    if (user.history.covid < 2):
+    if (user.history.covid_doses < 2):
         vac = Vaccine.objects.get(name="covid")
         turnoC = Appointment(state=0,center=user.center,vaccine=vac,patient=user)
         turnoC.save()
